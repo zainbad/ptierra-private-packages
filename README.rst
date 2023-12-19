cf90e4de006a1919eaa93fb636e8dc463fb69141
Zain Virk <zain.virk@arbisoft.com>
2023-12-19 11:17:57 +0500

    initial commit for tierra private packages plugin

---
+++ README.rst
@@ -0,0 +1,22 @@
+ptierra-private-packages plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/myusername/ptierra-private-packages
+
+Usage
+-----
+
+::
+
+    tutor plugins enable ptierra-private-packages
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,61 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutor_ptierra_private_packages", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="ptierra-private-packages",
+    version=ABOUT["__version__"],
+    url="https://github.com/myusername/ptierra-private-packages",
+    project_urls={
+        "Code": "https://github.com/myusername/ptierra-private-packages",
+        "Issue tracker": "https://github.com/myusername/ptierra-private-packages/issues",
+    },
+    license="AGPLv3",
+    author="ZV",
+    description="ptierra-private-packages plugin for Tutor",
+    long_description=load_readme(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["tutor"],
+    entry_points={
+        "tutor.plugin.v1": [
+            "ptierra-private-packages = tutor_ptierra_private_packages.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
